c7004893fc77da7582d172381f82cb9ff82fa2d0
yohannes57 <ytmderes@gmail.com>
2024-05-14 19:00:27 -0700

    all have been tried

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 console.log("dom manpulation lab....**");
-//*******_1_ */
+//**************************************_1_ */
 /*
 Select and cache the <main> element in a variable named mainEl.
 Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
@@ -9,22 +9,67 @@ Add a class of flex-ctr to mainEl.
 Hint: Use the Element.classList API.
 */
 
+let mainEl = document.getElementsByTagName("main");
+console.log(mainEl);
 
-
-let mainEl=document.getElementsByTagName('main');
-console.log(mainEl)
-
-mainEl[0].style.backgroundColor="var(--main-bg)"
-mainEl[0].style.borderTop="0px"
+mainEl[0].style.backgroundColor = "var(--main-bg)";
 
 // mainEl[0].innerHTML=`<h1>Dom manipulation </h1>`
-let heading=document.createElement('h1');
-heading.innerHTML="DOM MANUPULATION LAB";
-console.log(heading)
+let heading = document.createElement("h1");
+heading.innerHTML = "DOM MANUPULATION LAB";
+console.log(heading);
 mainEl[0].appendChild(heading);
 //
-mainEl[0].classList.add('flex-ctr');
-let body=document.getElementsByName('body');
-body[0].style.backgroundColor="var(--top-menu-bg)"
+mainEl[0].classList.add("flex-ctr");
 
-// mainEl[0,parentNode].style.backgroundColor="var(--main-bg)"
+//******************************************************___2____ */
+/*
+Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+Set the height of the topMenuEl element to be 100%.
+Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+Add a class of flex-around to topMenuEl.
+*/
+let topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+//*****************************************************_______3_____ */
+// Menu data structure
+
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+/*
+// Iterate over the entire menuLinks array and for each "link" object:
+// Create an <a> element.
+// On the new element, add an href attribute with its value set to the href property of the "link" object.
+// Set the new element's content to the value of the text property of the "link" object.
+// Append the new element to the topMenuEl element.
+*/
+
+// let topMenuEl = document.getElementById("top-menu");
+function assignLinks(links) {
+  links.forEach((link) => {
+    let aLink = document.createElement("a");
+    aLink.textContent = link.text;
+    aLink.href = link.href;
+
+    topMenuEl.appendChild(aLink);
+  });
+}
+assignLinks(menuLinks);
+
+///*********************************************______4________** */
+function assignLinks4(links) {
+  links.forEach((link) => {
+    let aLink = document.createElement("a");
+    aLink.textContent = link.text;
+    aLink.href = link.href;
+    aLink.classList.add("active");
+  });
+}
